0bda7049143fb965443a75bfa9724ad92d60ecf2
Ibitayo Akinnibosun <ibitayo.akinnibosun@gmail.com>
2025-01-31 22:54:50 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({
+        email: "ibitayo.akinnibosun@gmail.com",
+        current_datetime: new Date().toISOString(),
+        github_url: "https://github.com/ebtaryoh" 
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
